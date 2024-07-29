73a1891fd0618260aaba804207f1567700baf269
Abhay838 <singhabhay1838@gmail.com>
2024-07-29 16:00:59 +0530

    changes1

--- lib/src/model/model_1.dart
+++ lib/src/model/model_1.dart
@@ -1,34 +1,60 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:uuid/uuid.dart';
+
 class Contact {
+  final String id;
   final String name;
 
-  const Contact({required this.name});
+  Contact({required this.name}) : id = const Uuid().v4();
+  Contact.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        name = json['name'];
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'name': name,
+      };
 }
 
-class ContactBook {
+class ContactBook extends ValueNotifier<List<Contact>> {
   // singleton
-  ContactBook._sharedInstance();
+  ContactBook._sharedInstance() : super([]) {
+    _loadContacts();
+  }
   static final ContactBook _shared = ContactBook._sharedInstance();
 
   factory ContactBook() => _shared;
 
-  // Changed to a mutable list
-  final List<Contact> _contact = [
-    Contact(name: 'Trupti'),
-    Contact(name: 'Abhay'),
-    Contact(name: 'Vivek'),
-    Contact(name: 'Bobby'),
-  ];
-
-  int get length => _contact.length;
+  int get length => value.length;
 
   void add({required Contact contact}) {
-    _contact.add(contact);
+    value.add(contact);
+    notifyListeners();
+    _saveContacts();
   }
 
   void remove({required Contact contact}) {
-    _contact.remove(contact);
+    value.remove(contact);
+    notifyListeners();
+    _saveContacts();
   }
 
   Contact? contact({required int atIndex}) =>
-      _contact.length > atIndex ? _contact[atIndex] : null;
+      value.length > atIndex ? value[atIndex] : null;
+
+  void _loadContacts() async {
+    final prefs = await SharedPreferences.getInstance();
+    final contactsJson = prefs.getString('contacts') ?? '[]';
+    final List<dynamic> contactsList = jsonDecode(contactsJson);
+    value = contactsList.map((json) => Contact.fromJson(json)).toList();
+  }
+
+  void _saveContacts() async {
+    final prefs = await SharedPreferences.getInstance();
+    final contactsJson = jsonEncode(value.map((c) => c.toJson()).toList());
+    prefs.setString('contacts', contactsJson);
+  }
 }
--- lib/src/page/page_1.dart
+++ lib/src/page/page_1.dart
@@ -14,30 +14,47 @@ class _Page1State extends State<Page1> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.tealAccent,
-        title: const Text("Contact Home"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.tealAccent.shade100,
-        onPressed: () async {
-          await Navigator.pushNamed(context, '/add');
-        },
-        child: const Icon(
-          Icons.add,
-          size: 30,
-          color: Colors.grey,
+        appBar: AppBar(
+          backgroundColor: Colors.tealAccent,
+          title: const Text("Contact Home"),
         ),
-      ),
-      body: ListView.builder(
-        itemCount: contactBook.length,
-        itemBuilder: (context, index) {
-          final contact = contactBook.contact(atIndex: index)!;
-          return ListTile(
-            title: Text(contact.name),
-          );
-        },
-      ),
-    );
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.tealAccent.shade100,
+          onPressed: () async {
+            await Navigator.pushNamed(context, '/add');
+          },
+          child: const Icon(
+            Icons.add,
+            size: 30,
+            color: Colors.grey,
+          ),
+        ),
+        body: ValueListenableBuilder<List<Contact>>(
+          valueListenable: ContactBook(),
+          builder: (contact, contacts, child) {
+            return ListView.builder(
+              itemCount: contacts.length,
+              itemBuilder: (context, index) {
+                final contact = contacts[index];
+                return Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
+                  child: Dismissible(
+                    onDismissed: (direction) {
+                      contactBook.remove(contact: contact);
+                    },
+                    key: ValueKey(contact.id),
+                    child: Material(
+                      elevation: 6,
+                      color: Colors.white,
+                      child: ListTile(
+                        title: Text(contact.name),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          },
+        ));
   }
 }
